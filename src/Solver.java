d6177b5aee621349614c1f05c598ed46d3f57bcd
Rob Robertson <rsraloha@gmail.com>
2017-07-12 08:05:52 -0400

    solver

--- src/Solver.java
+++ src/Solver.java
@@ -1,33 +1,54 @@
 import edu.princeton.cs.algs4.MinPQ;
-import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.SET;
 import java.util.Comparator;
+import java.util.HashSet;
 
 /**
  * Created by rsraloha on 5/23/17.
  */
 public class Solver {
 
+    private boolean _isSolvable;
+    private Queue _gameTree;
+    private int _numMoves = -1;
+
     public Solver(Board initial) {
 
         Board twin = initial.twin();
-        Stack<Board> test = new Stack();
-        Stack<Board> gameTree = new Stack();
-        MinPQ<Board> pq = new MinPQ(new ManhattanComparator());
-        MinPQ<Board> pqTwin = new MinPQ(new ManhattanComparator());
-        pq.insert(initial);
+        _gameTree = new Queue<Board>();
+        MinPQ frontier = new MinPQ(new ManhattanComparator());
+        Queue gameTreeTwin = new Queue();
+        MinPQ frontierTwin = new MinPQ(new ManhattanComparator());
 
-        pq.insert(initial);
-        pqTwin.insert(twin);
-        while (!pq.isEmpty()) {
-            Board node = pq.delMin();
+        GameNode start = new GameNode(initial,0);
+        frontier.insert(start);
+        GameNode startTwin = new GameNode(twin,0);
+        frontierTwin.insert(startTwin);
 
-            gameTree.push(node);
-            if (node.isGoal()) {
-                return;
+        while (true) {
+            GameNode node = (GameNode)frontier.delMin();
+            GameNode nodeTwin = (GameNode)frontierTwin.delMin();
+            _numMoves++;
+
+            _gameTree.enqueue(node.board);
+            if (node.board.isGoal()) {
+                _isSolvable = true;
+                break;
+            }
+            if (nodeTwin.board.isGoal()) {
+                _isSolvable = false;
+                break;
             }
 
-            for (Board b : node.neighbors()) {
-                pq.insert(b);
+
+            for (Board n : node.board.neighbors()) {
+                GameNode next = new GameNode(n,node.numMoves + 1);
+                frontier.insert(next);
+            }
+            for (Board n : nodeTwin.board.neighbors()) {
+                GameNode next = new GameNode(n,nodeTwin.numMoves + 1);
+                frontierTwin.insert(next);
             }
 
         }
@@ -35,15 +56,15 @@ public class Solver {
     }
 
     public boolean isSolvable() {
-        return true;
+        return _isSolvable;
     }
 
     public int moves() {
-        return 0;
+        return _numMoves;
     }
 
     public Iterable<Board> solution() {
-        return new Stack<Board>();
+        return _gameTree;
     }
 
     public static void main(String[] args) {
@@ -51,17 +72,34 @@ public class Solver {
 
     }
 
-    private class ManhattanComparator implements Comparator<Board> {
-        @Override
-        public int compare(Board a, Board b) {
-            return Integer.compare(a.manhattan(), b.manhattan());
+    private class GameNode  {
+        public GameNode(Board b, int moves){
+            board = b;
+            manhattan = b.manhattan();
+            numMoves = moves;
+            priority = manhattan + numMoves;
+        }
+        int manhattan;
+        int numMoves;
+        int priority;
+        Board board;
 
+        public boolean equals(Object other){
+            if(this == other)
+                return true;
+            if(other == null)
+                return false;
+            if (other.getClass() != this.getClass()) return false;
+
+            GameNode that = (GameNode) other;
+            return this.board.equals(that.board);
         }
     }
-    private class HammingComparator implements Comparator<Board> {
+
+    private class ManhattanComparator implements Comparator<GameNode> {
         @Override
-        public int compare(Board a, Board b) {
-            return Integer.compare(a.hamming(), b.hamming());
+        public int compare(GameNode a, GameNode b) {
+            return Integer.compare(a.priority, b.priority);
 
         }
     }
